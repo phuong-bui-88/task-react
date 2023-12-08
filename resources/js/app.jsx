58147c03ff70d51e1d9c0a2a8e9904a91fd2f2bc
phuong.bui <phuong.bui@haegroup.com>
2023-12-08 17:53:53 +0700

    first laravel-ui

---
+++ resources/js/app.jsx
@@ -0,0 +1,7 @@
+import './bootstrap';
+
+import React from "react";
+import ReactDOM from "react-dom";
+import ExampleComponent from "./components/ExampleComponent.jsx";
+
+ReactDOM.render(<ExampleComponent />, document.getElementById("app"))
---
+++ resources/js/components/ExampleComponent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ExampleComponent() {
+    return (
+        <div>
+            <h1>Hello word</h1>
+        </div>
+    )
+}
+
+export default ExampleComponent
